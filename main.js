9bebb1a9e1189c85865376a87c9cfc1e13196bc3
jorgev259 <jorgev259@outlook.es>
2019-11-04 19:42:01 -0600

    first commit

--- main.js
+++ main.js
@@ -6,14 +6,26 @@ $(document).ready(() => {
     .year(2019).month(9).date(29).hour(10).minute(0).second(0)
   const today = moment.tz('America/Los_Angeles')
   const todayReset = moment.tz('America/Los_Angeles').hour(10).minute(0).second(0)
+  const reset1 = todayReset.clone()
+  const reset2 = todayReset.clone()
+  reset1.add(1, 'day')
+  reset2.add(2, 'day')
 
   let diff = moment.tz('America/Los_Angeles').diff(start, 'days')
   const afterReset = today.isAfter(todayReset)
   console.log(afterReset)
-  if (afterReset) diff++
+  if (!afterReset) diff += -1
 
   const currentWeapon = weapons[diff % 3]
   console.log(currentWeapon)
 
   $('#weapon1').text(currentWeapon)
+
+  weapons.splice(diff % 3, 1)
+
+  $('#timer2').text(today.to(reset1))
+  $('#weapon2').text(weapons[diff % 3])
+  weapons.splice(diff % 3, 1)
+  $('#timer3').text(today.to(reset2))
+  $('#weapon3').text(weapons[0])
 })
